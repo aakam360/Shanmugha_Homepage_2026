505eaca96d292ca6690e1d04342fb8a7172e1c5c
FathimaJinan <e23ai036@shanmugha.edu.in>
2026-01-22 16:37:46 +0530

    build update

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  turbopack: {
+    root: '.', // Relative path to the workspace root
+  },
 };
 
 export default nextConfig;
